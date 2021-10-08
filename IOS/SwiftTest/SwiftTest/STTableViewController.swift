fdb3d0b2802c054d05d7fc2d58f33f750b561cbe
莫天乐 <tinlemo@kugou.net>
2021-10-08 23:14:12 +0800

    swift学习

---
+++ IOS/SwiftTest/SwiftTest/STTableViewController.swift
@@ -0,0 +1,44 @@
+//
+//  STTableViewController.swift
+//  SwiftTest
+//
+//  Created by Motinle on 2021/10/8.
+//
+
+import UIKit
+
+class STTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.tableView.backgroundColor = UIColor.purple
+        
+        
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        cell.textLabel?.text = "flutter"
+
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 0 {
+            self.performSegue(withIdentifier: "pushFlutter", sender: self)
+        }
+    }
+
+}
+
